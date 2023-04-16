66c428ee911fc73065a0e5cb124b3eaed604f902
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-04-16 16:49:55 -0600

    API creacion de historiales

--- routes/record.route.js
+++ routes/record.route.js
@@ -4,5 +4,10 @@ var condition = require('../controllers/record.controller');
 var api = express.Router();
 
 api.post('/createIncidencia',condition.createIncidence);
+api.post('/createHistorial_Enfermedad',condition.createDiseaseRecord);
+api.post('/createHistorial_Adiccion',condition.createAddictionRecord);
+api.post('/createHistorial_Examen',condition.createExamRecord);
+api.post('/createHistorial_Cirugia',condition.createSurgeryRecord);
+api.post('/createHistorial_Tratamiento',condition.createTreatmentRecord);
 
 module.exports = api;
