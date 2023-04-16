adce9d5fcb978e453e5575b280a1b62c070cb06f
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-04-16 11:04:48 -0600

    API Creacion Incidencia en Historial Medico

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ var materialRoutes = require('./routes/material.route');
 var directionRoutes = require('./routes/direction.route');
 var conditionRoutes = require('./routes/condition.route');
 var practiceRoutes = require('./routes/practice.route');
+var recordRoutes = require('./routes/record.route');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
@@ -37,5 +38,6 @@ app.use(materialRoutes);
 app.use(directionRoutes);
 app.use(conditionRoutes);
 app.use(practiceRoutes);
+app.use(recordRoutes);
 
 module.exports = app;
