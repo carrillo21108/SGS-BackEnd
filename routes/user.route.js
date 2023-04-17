9a4bfdddbd105d6b64aa5c14edaa0f4aa9837973
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-04-17 09:29:04 -0600

    API get Resumen de expediente

--- routes/user.route.js
+++ routes/user.route.js
@@ -7,5 +7,6 @@ api.post('/login',userController.login);
 api.post('/createPaciente',userController.createPatient);
 api.post('/createMedico',userController.createDoctor);
 api.post('/createEstado',userController.createState);
+api.get('/getResumenExpediente',userController.getSummaryFile);
 
 module.exports = api;
