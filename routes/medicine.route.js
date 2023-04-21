574bba96e57e6411c939872241a41409feb5d052
isaackeitor <josuem81010@gmail.com>
2023-04-20 19:29:31 -0600

    GetMedicines

--- routes/medicine.route.js
+++ routes/medicine.route.js
@@ -4,5 +4,6 @@ var medicine = require('../controllers/medicine.controller');
 var api = express.Router();
 
 api.post('/createMedicamento',medicine.create);
+api.get('/getMedicamentos',medicine.getMedicines);
 
 module.exports = api;
