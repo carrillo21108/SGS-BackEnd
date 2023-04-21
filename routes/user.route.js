595d8e6574e895a555d54f79b475e96cc3077ed1
carrillo21108 <87512111+carrillo21108@users.noreply.github.com>
2023-04-20 18:27:21 -0600

    getMedicos

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,17 +8,18 @@ api.post('/createPaciente',userController.createPatient);
 api.post('/createMedico',userController.createDoctor);
 api.post('/createEstado',userController.createState);
 
-api.get('/getResumenExpediente',userController.getSummaryFile);
-api.get('/getEnfermedadesIncidencia',userController.getDiseaseIncidence);
-api.get('/getExamenesIncidencia',userController.getExamIncidence);
-api.get('/getAdiccionesIncidencia',userController.getAddictionIncidence);
-api.get('/getCirugiasIncidencia',userController.getSurgeryIncidence);
-api.get('/getTratamientoIncidencia',userController.getTreatmentIncidence);
+api.post('/getResumenExpediente',userController.getSummaryFile);
+api.post('/getEnfermedadesIncidencia',userController.getDiseaseIncidence);
+api.post('/getExamenesIncidencia',userController.getExamIncidence);
+api.post('/getAdiccionesIncidencia',userController.getAddictionIncidence);
+api.post('/getCirugiasIncidencia',userController.getSurgeryIncidence);
+api.post('/getTratamientoIncidencia',userController.getTreatmentIncidence);
 
 api.post('/getPaciente',userController.getPatient);
 api.post('/getMedico',userController.getDoctor);
 api.get('/getEstados',userController.getStates);
 api.post('/getPosiblesPadres',userController.getPossibleParents);
+api.get('/getMedicos',userController.getDoctors);
 
 api.put('/transferPersona',userController.transferPerson);
 api.put('/updatePaciente',userController.updatePatient);
